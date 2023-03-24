f9f7426c8bbde5584c9010fe6d5426324f8a4cf7
BryanQuintanaTV <18308051220409@cbtis122.edu.mx>
2023-03-23 22:43:03 -0600

    Almost end

--- src/NewMain.java
+++ src/NewMain.java
@@ -14,23 +14,48 @@ public class NewMain {
      */
     public static void main(String[] args) {
         Persona persona = new Persona();
-        
-        System.out.println("here");
+
         persona.setCelular("42584");
         persona.setEstadoDeNacimiento("Chihuahua");
-        persona.setFechaNacimiento(new Fecha());
+        persona.setFechaNacimiento(new Fecha(18, 4, 2003));
         persona.setMaterno("Juarez");
         persona.setNombre("Bryan");
         persona.setPaterno("Quintana");
         String sexo = "H";
         persona.setSexo(sexo.charAt(0));
-        System.out.println("here 2");
-        persona.setCurp("QUJB030418HC");
+        persona.setCurp("QUJB030418HCHNNRB8");
         
-        System.out.println("La curp es: " +persona.getCurp());
+        Persona persona2 = new Persona();
+
+        persona2.setCelular("423584");
+        persona2.setEstadoDeNacimiento("Chihuahua");
+        persona2.setFechaNacimiento(new Fecha(17, 10, 2003));
+        persona2.setMaterno("Monge");
+        persona2.setNombre("Iran");
+        persona2.setPaterno("Chavarria");
+        String sexoAme = "M";
+        persona2.setSexo(sexoAme.charAt(0));
+        persona2.setCurp("CAMI031017MCHHNRA5");
+
+
         
-        System.out.println(persona.validarPaterno("Quintana")+persona.validarMaternoNombre());
+        System.out.println("Validar Curp: "+ persona.validarCurp("QUJB030418HCHNRRB8"));
+        System.out.println("Validar Curp: "+ persona2.validarCurp("CAMI031017MCHHNRA5"));
         
+        
+        
+        /*
+        String s = "Bryan100";
+        String newS = s.substring(5, 6);
+
+        System.out.println("NewS is: " + newS);
+        boolean isNumeric = (newS != null && newS.matches("[0-9]+"));
+        System.out.println("IsNumeric: " + isNumeric);
+
+        String ss = "abcdefg";
+        boolean isOnlyLetters = (ss != null && ss.matches("^[a-zA-Z]+$"));
+        System.out.println("IsOnlyLetters: " + isOnlyLetters);*/
+
     }
-    
+
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,8 +1,10 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * "
  * "
  */
-
 /**
  *
  * @author Quintana Juarez Bryan Alexis
@@ -17,7 +19,6 @@ public class Persona {
     private Fecha fechaNacimiento;
     private String estadoDeNacimiento;
     private char sexo;
-    private char[] arregloCurp = new char[13];
 
     public Persona() {
     }
@@ -32,70 +33,15 @@ public class Persona {
         this.estadoDeNacimiento = estadoDeNacimiento;
         this.sexo = sexo;
     }
-    
-    private char[] arregloCurp(){
-        
-        if (curp.length() == 13) {
-            for (int i = 0; i < 13; i++) {
-                arregloCurp[i] = curp.charAt(i);
-            }
-        }
-        
-        return arregloCurp;
-    }
 
     public String getCurp() {
-        
-        
-        return validarCurp("ujh");
+
+        return "";
     }
 
     public void setCurp(String curp) {
-
         this.curp = curp;
-        arregloCurp();
-    }
-
-    private String validarCurp(String curp) {
-
-        return "";
-        
-    }
-    
-    public String validarPaterno(String paterno){
-        
-        char apellidoPrimeraLetra = paterno.charAt(0);
-
-        String cadena = paterno.substring(1);
-        String newCadena="";
-        String paternoFinal = "";
-        boolean vocal = false;
-        int i = 0;
-        while (i < cadena.length() & !vocal) {
-            if ((cadena.charAt(i) == 'a')
-                    || (cadena.charAt(i) == 'e')
-                    || (cadena.charAt(i) == 'i')
-                    || (cadena.charAt(i) == 'o')
-                    || (cadena.charAt(i) == 'u')) {
-
-                newCadena = (newCadena  + cadena.charAt(i)).toUpperCase();
-                vocal = true;
-                i++;
-            }
-
-        }
-        paternoFinal = apellidoPrimeraLetra+newCadena;
-        return paternoFinal;
-        
-    }
-    public String validarMaternoNombre(){
-        
-        char apellidoPrimeraLetra = materno.charAt(0);
-        char nombrePrimeraLetra = nombre.charAt(0);
-        
-        String fin = ""+apellidoPrimeraLetra+nombrePrimeraLetra;
-        
-        return fin;
+        validarCurp(curp);
     }
 
     public String getPaterno() {
@@ -153,5 +99,216 @@ public class Persona {
     public void setSexo(char sexo) {
         this.sexo = sexo;
     }
+
+    public String validarCurp(String curp) {
+        if (validarPaterno(curp) && validarMaternoNombre(curp) && validarFechaNac(curp) && validarSexo(curp) && validarEstado(curp) && validarConstantes(curp) && verificarHomonimia(curp) && verificarDigito(curp)) {
+            return "Curp valida";
+        } else {
+            return "Curp NO valida";
+        }
+    }
+
+    //Métodos para validar la curp ------------------------------------------------
+    //Método para validar las posiciones de la curp 1 y 2
+    private boolean validarPaterno(String curp) {
+
+        char apellidoPrimeraLetra = paterno.charAt(0); //Trae la primera letra del apellido paterno
+        String curpValidar = curp.substring(0, 2); //trae las posiciones 1 y 2 de la curp a validar
+
+        String cadena = paterno.substring(1); //Trae el apellido paterno a partir de la segunda letra
+        String newCadena = "";
+        String cadenaFinal = "";
+        String paternoFinal = "";
+
+        boolean vocal = false;
+        int i = 0;
+        //Con el while logramos obtener la primera vocal del apellido paterno
+        while (i < cadena.length() & !vocal) {
+            if (cadena.charAt(i) == 'h') {
+                vocal = false;
+                i++;
+            } else if ((cadena.charAt(i) == 'a')
+                    || (cadena.charAt(i) == 'e')
+                    || (cadena.charAt(i) == 'i')
+                    || (cadena.charAt(i) == 'o')
+                    || (cadena.charAt(i) == 'u')) {
+
+                newCadena = (newCadena + cadena.charAt(i));
+                vocal = true;
+                i++;
+            }
+
+        }
+        cadenaFinal = newCadena.toUpperCase();
+        paternoFinal = apellidoPrimeraLetra + cadenaFinal;
+        //validamos que las dos primeras posiciones coincidan
+        if (curpValidar.equals(paternoFinal)) {
+            return true;
+        }
+        return false;
+    }
+
+    //Método que verifica la posicion 3 y 4 de la curp
+    private boolean validarMaternoNombre(String curp) {
+
+        char apellidoPrimeraLetra = materno.charAt(0);
+        char nombrePrimeraLetra = nombre.charAt(0);
+        String curpValidar = curp.substring(2, 4); //trae las posiciones 3 y 4 de la curp a validar
+        String valor = String.valueOf(apellidoPrimeraLetra) + String.valueOf(nombrePrimeraLetra);
+
+        if (curpValidar.equals(valor)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    //Método para verificar las posiciones de la 5 a la 10
+    private boolean validarFechaNac(String curp) {
+        String curpValidar = curp.substring(4, 10); //trae las posiciones 5-10 de la curp a validar
+        String monthTxt = "";
+        String dayTxt = "";
+        String validarFinal = "";
+
+        int day = fechaNacimiento.getDia();
+        if (day < 10) {
+            dayTxt = "0" + day;
+        } else {
+            dayTxt = String.valueOf(day);
+        }
+        int month = fechaNacimiento.getMes();
+        if (month < 10) {
+            monthTxt = "0" + month;
+        } else {
+            monthTxt = String.valueOf(month);
+        }
+        String year = String.valueOf(fechaNacimiento.getAnio());
+        String yearTxt = year.substring(2);
+
+        validarFinal = "" + yearTxt + monthTxt + dayTxt;
+        if (validarFinal.equals(curpValidar)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    //Método para validar la posición 11
+    private boolean validarSexo(String curp) {
+
+        String sexoChecar = String.valueOf(sexo);
+        String curpValidar = curp.substring(10, 11); //trae las posiciones 11 de la curp a validar
+
+        if (("H".equals(sexoChecar) || "M".equals(sexoChecar)) && ("H".equals(curpValidar) || "M".equals(curpValidar))) {
+            return true;
+        }
+        return false;
+    }
+    
+    //Método para validar la posiciones 12-13
+    private boolean validarEstado(String curp) {
+        String estado = "";
+        String estadoTemp = "";
+        String curpValidar = curp.substring(11, 13); //trae las posiciones 12-13 de la curp a validar
+
+        for (int i = 0; i < estadoDeNacimiento.length(); i++) {
+            char ch = estadoDeNacimiento.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                
+            } else if (ch != ' ') {
+                estadoTemp = estadoTemp + ch;
+            }
+        }
+        estadoTemp = (estadoTemp.substring(estadoTemp.length() - 1));
+
+        estado = "" + estadoDeNacimiento.charAt(0) + estadoTemp;
+        estado = estado.toUpperCase();
+        if (estado.equals(curpValidar)) {
+            return true;
+        }
+        return false;
+
+    }
+    //Método para validar las posiciones 14-16
+    private boolean validarConstantes(String curp) {
+
+        String name = "";
+        String ApellidoMaterno = "";
+        String ApellidoPaterno = "";
+        
+        String curpValidar = curp.substring(13, 16); //trae las posiciones 12-13 de la curp a validar
+        
+        String nameTemp = "";
+        String ApellidoMaternoTemp = "";
+        String ApellidoPaternoTemp = "";
+        String resulFinal = "";
+
+        for (int i = 0; i < nombre.length(); i++) {
+            char ch = nombre.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.print("");
+            } else if (ch != ' ') {
+                nameTemp = nameTemp + ch;
+            }
+        }
+        name = (String.valueOf(nameTemp.charAt(1))).toUpperCase();
+
+        for (int i = 0; i < materno.length(); i++) {
+            char ch = materno.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.print("");
+            } else if (ch != ' ') {
+                ApellidoMaternoTemp = ApellidoMaternoTemp + ch;
+            }
+        }
+        ApellidoMaterno = (String.valueOf(ApellidoMaternoTemp.charAt(1))).toUpperCase();
+
+        for (int i = 0; i < paterno.length(); i++) {
+            char ch = paterno.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.print("");
+            } else if (ch != ' ') {
+                ApellidoPaternoTemp = ApellidoPaternoTemp + ch;
+            }
+        }
+        ApellidoPaterno = (String.valueOf(ApellidoPaternoTemp.charAt(1))).toUpperCase();
+        resulFinal = ApellidoPaterno + ApellidoMaterno + name;
+        
+        if (resulFinal.equals(curpValidar)) {
+            return true;
+        }
+        
+        return false;
+
+    }
+    //Métod para verificar el diferenciador de homonimia y siglo "Posición 17"
+    private boolean verificarHomonimia(String curp){
+        
+        String curpValidar = curp.substring(16, 17); //trae las posiciones 12-13 de la curp a validar
+        int year = fechaNacimiento.getAnio();
+        
+        if (year <= 1999) {
+            if (curpValidar.matches("[0-9]+")) {
+                return true;
+            }
+        }else if (year >= 2000) {
+            if (curpValidar.matches("[A-Z]*")) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    //Método para verificar dígito verificador
+    private boolean verificarDigito(String curp){
+        
+        String curpValidar = curp.substring(17, 18); //trae las posiciones 12-13 de la curp a validar
+        
+        if (curpValidar.matches("[0-9]+")) {
+                return true;
+            }
+        return false;
+    }
+//Fin de métodos para validar la curp ------------------------------------------------
 
 }
